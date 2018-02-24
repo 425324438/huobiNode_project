11ec9761c4d5ac00b1566f89823a6b2bc1039b52
shiyalong <425324438@qq.com>
2018-02-24 18:03:16 +0800

    'up'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('进入 根路径');
+  res.render('index.html', { title: 'Express' });
 });
 
 module.exports = router;
