4700893e4e9b04c92ec5a7d1f060a9f514c9fa52
暮色听雨声 <425324438@qq.com>
2018-02-25 11:36:32 +0800

    up

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/add',(req, res, next)=>{
+  console.log(`进入了 POST 方法`);
+  console.log(req.query.user);
+});
+
 module.exports = router;
