13e77db0439df10be89a2be5b19b7750035ff452
暮色听雨声 <425324438@qq.com>
2018-02-27 22:32:40 +0800

    up

---
+++ bin/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "config": {
+        "redisHost": "",
+        "redisPort": "6379"
+    }
+}
--- test.js
+++ test.js
@@ -1,32 +1,17 @@
 // const hbsdk = require('./sdk/hbsdk');
 // const sendMail = require('./email/mail');
-
+const fs = require('fs');
 // let client = require('../sdk/redisClient');
 
 let  mainTest  = async (account) =>  {
     
     // sendMail('2037520355@qq.com','NodeJs测试','数字货币价格监控测试');
-
-    // let num = 1.726273600000000000;
-    // let str = JSON.stringify(num);
-    // Math.round(123.4567789*100)/100
     
-    // client.smembers('userEmail',(err,result)=>{
-    //     console.log(result);
-    // });
-    // let dateString = '2018-02-16 10:20:00';
-    // let zaodate = new Date().convertDateFromString(dateString);
+    // let str = '{"btcusdt":"9445.63","dataTime":"2018-02-25 09:48:55"}';
 
-    // let wandate = new Date().convertDateFromString('2018-02-16 10:25:00');
-    // console.log(zaodate.getTime());
-    // console.log(wandate.getTime());
+    // console.log(JSON.parse(str));
+    
 
-    // console.log(zaodate.getTime() - wandate.getTime());
-    // console.log(`${1000 * 60 * 5} ：5分钟的毫秒数`);
-
-    let str = '{"btcusdt":"9445.63","dataTime":"2018-02-25 09:48:55"}';
-
-    console.log(JSON.parse(str));
 }
 
 
@@ -37,4 +22,3 @@ Date.prototype.convertDateFromString = function convertDateFromString(dateString
     }
 }
 mainTest();
-// module.exports =  mainTest;
